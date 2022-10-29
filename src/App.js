4cea34aed9ef30f094072352234d6460e8fb017e
unknown <shanelwilliams2018@gmail.com>
2022-10-28 18:33:03 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom'
+import './App.scss'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Particles from 'react-tsparticles'
+import { loadFull } from 'tsparticles'
+import Home from './containers/home'
+import About from './containers/about'
+import Resume from './containers/resume'
+import Skills from './containers/skills'
+import Portfolio from './containers/portfolio'
+import Contact from './containers/contact'
+import Navbar from './components/navBar'
+import { particlesConfig } from './helpers/particlesConfig'
+import Theme from './components/theme'
 
 function App() {
-  return (
-    <div className="App">
-     {/* particle js */}
+	const particlesInit = async (main) => {
+		await loadFull(main)
+	}
 
-     {/* navbar component */}
+	const location = useLocation()
+	const renderParticleHomePage = location.pathname === '/'
 
-     {/* main page content */}
-     <Routes>
-      {/* create all routes */}
-      <Route path='/' index element={<Home />} />
-      <Route path='/about' index element={<About />} />
-      <Route path='/resume' index element={<Resume />} />
-      <Route path='/skills' index element={<Skills />} />
-      <Route path='/portfolio' index element={<Portfolio />} />
-      <Route path='/contact' index element={<Contact />} />
-     </Routes>
-    </div>
-  );
+	return (
+		<div className='App'>
+			{/* particle js */}
+			{renderParticleHomePage && (
+				<Particles
+					id='particles'
+					options={particlesConfig}
+					init={particlesInit}
+				/>
+			)}
+			{/* navbar component */}
+			<div className='App__navbar-wrapper'></div>
+			<Navbar />
+			{/* main page content */}
+			<div className='App__main-content-wrapper'>
+				<Theme />
+				<Routes>
+					{/* create all routes */}
+					<Route path='/' index element={<Home />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/skills' element={<Skills />} />
+					<Route path='/resume' element={<Resume />} />
+					<Route path='/portfolio' element={<Portfolio />} />
+					<Route path='/contact' element={<Contact />} />
+				</Routes>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
+import { FaLinkedin } from 'react-icons/fa'
+import { Animate } from 'react-simple-animate'
+import './styles.scss'
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
+	return (
+		<div>
+			<section className='home' id='home'>
+				<div className='home__text-wrapper'>
+					<h1>
+						Hello, I'm Shanel.
+						<br />
+						Frontend Web Developer
+					</h1>
+				</div>
+				<Animate
+					play
+					duration={1.5}
+					delay={1}
+					start={{
+						transform: 'translateY(600px)',
+					}}
+					end={{
+						transform: 'translateX(0px)',
+					}}
+				>
+					<div className='contact-me'>
+						<div className='contact-me__buttons-wrapper'>
+							<button>Hire me</button>
+							<a href={'https://docs.google.com/document/d/1iSjKCjIsQGKNn1Gl6RVQyyuRPSpslIpfjKgc_sU6RNk/edit?usp=sharing'}>Download Resume</a>
+						</div>
+						<div className='contact-me__social-wrapper'>
+							<FaLinkedin size={32} />
+						</div>
+					</div>
+				</Animate>
+			</section>
+		</div>
+	)
 }
 
 export default Home
